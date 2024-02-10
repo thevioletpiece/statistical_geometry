436d8c94cf48e1b07b1090d9b2cca797d7faad7a
Michael J Twardos <michael.twardos@gmail.com>
2024-02-10 06:58:55 -0800

    Add files via upload
    New work on monodisperse packing and percolation

--- Stat Geo Code.m
+++ Stat Geo Code.m
@@ -124,30 +124,34 @@ If[i1!=i2, Break];
 Sqrt[Total[(A[[#]]-B[[#]])^2&/@Range[i1]]]]
 
 
-generateThings[num_, radius_]:=Module[{i, things={{Random[], Random[]}}, position, distances},
+generateThings[num_, radius_]:=Module[{i, things={{Random[], Random[]}}, position, distances, dataz={}},
 For[i=1, i<num, 
-(* Generate a new set of coordinates for a new particle.*)
+(* Generate a new set of coordinates for a new particle in a box and make sure the edges of the circle 
+fit in the box. *)
 position={(1-2*radius)*Random[]+radius, (1-2*radius)*Random[]+radius};
 (* Compute the distances to all the existing particle things to determine which overlap *)
 distances=computeDistance[position, #]&/@things;
 L=Length[Select[distances, # < 2*radius &]];
 (* Things is the list of non intersecting spheres or other objects *)
 things=If[L==0, Append[things, position], things];
+dataz=Append[dataz, {i, Length[things]}];
 i++];
-things]
+(* Temporarily adding dataz in the output to explore runtimes *)
+{things, dataz}]
 
 
-determineDensity[DtoL_, worlds_]:=Module[{density={}, filling={}},
+determineDensity[DtoL_, worlds_, iterations_]:=Module[{density={}, filling={}},
 For[i=1,i< worlds,
 (* Instead of 100 attempts here, we should try proportional to the likelihood of continuing, or stop once we've had X failures. *)
 (* This is where efficiency comes in.  We need to give each scenario the same chances to converge. *)
- H=generateThings[300, DtoL/2]; 
+ H=generateThings[iterations, DtoL/2]; 
 density=Append[density, Length[H]*Pi*(DtoL/2)^2];
 filling=Append[filling, If[Length[H]>1, 1, 0]];
 i++];
 {Mean[density], Mean[filling]}]
 
 
+(* This function allowed us to research high aspect ratio containers *)
 generateThings2[num_, radius_, height_]:=Module[{i, things={{Random[], Random[]}}, position, distances},
 For[i=1, i<num, 
 position={(1-2*radius)*Random[]+radius, (height-2*radius)*Random[]+radius};
@@ -170,3 +174,95 @@ For[i=1, i<1000,
 isIn=Append[isIn, pointInCircleS[{xSlice, i/100}, Q, radius]];
 i++];
 N[Total[isIn]/1000]]
+
+
+isOverlapping[{positionA_, radiusA_, positionB_, radiusB_}]:=Module[{D},
+D=computeDistance[positionA, positionB];
+If[D<(radiusA+radiusB), 1, 0]
+]
+
+
+makeBidispers[num_, radiusA_,radiusB_, pA_]:=Module[{i, things={{{Random[], Random[]}, If[Random[]<pA, radiusA, radiusB]}}, position,radius={},  distances},
+For[i=1, i<num,
+radius=If[pA>Random[], radiusA, radiusB];
+position={(1-2*radius)*Random[]+radius, (1-2*radius)*Random[]+radius};
+distances=isOverlapping[{position, radius,  #[[1]], #[[2]]}]&/@things;
+L=Total[distances];
+things=If[L==0, Append[things,{ position, radius}], things];
+i++];
+things]
+
+
+fillGrid[size_, iterations_]:=Module[{filled, point},
+filled={{1,1}};
+While[ Length[filled]<iterations, 
+point={Floor[(size-1)*Random[] + 1], Floor[(size-1)*Random[] + 1]};
+filled=If[Not[MemberQ[filled, point] ],Append[filled, point], filled]; 
+];
+filled]
+
+
+isPieceAdjacent[pieceA_, pieceB_]:=MemberQ[{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}, pieceA-pieceB]
+
+
+getAdjacentPieces[item_, available_]:=#[[1]]&/@Select[{#, isPieceAdjacent[item, #]}&/@available, #[[2]]==True &]
+
+
+growShape[items_, newObject_, available_]:=Module[{connectingItems, newerObject, newAvailable, newItems},
+newAvailable=available;
+(* newAvailable=If[Length[items]\[Equal]1, Delete[newAvailable, Position[newAvailable, items[[i]]]], newAvailable]; *)
+For[i = 1, i<=Length[items], i++,
+newAvailable=Delete[newAvailable, Position[newAvailable, items[[i]]]];
+];
+ newItems=Flatten[getAdjacentPieces[#, newAvailable]&/@items];
+connectingItems=If[Length[newItems]>0,Complement[ Partition[newItems, 2]], newItems];
+newerObject=newObject;
+For[i = 1, i<=Length[connectingItems], i++,
+newerObject=Append[newerObject, connectingItems[[i]]];
+newAvailable=Delete[newAvailable, Position[newAvailable, connectingItems[[i]]]];
+]; 
+{connectingItems, newerObject, newAvailable}]
+
+
+computeLargestPiece[size_, iterations_]:=Module[{available, objects = {},  item, connectingItems, newObject},
+available=fillGrid[size, iterations];
+item =available[[1]];
+connectingItems={item};
+newObject = {item};
+available=Delete[available, Position[available, item]];
+
+While[Length[available]> 1, 
+While[Length[connectingItems]>0,
+{connectingItems, newObject, available}=growShape[connectingItems, newObject, available]];
+objects=Append[objects, Complement[newObject]];
+item =available[[1]];
+connectingItems={item}; 
+newObject = {item};
+];
+Sort[Length[#]&/@objects, #1>#2 &][[1]]
+]
+
+
+getMaxY[itemEffected_, verticalConcern_]:=Module[{columns},
+columns=Select[itemEffected, MemberQ[verticalConcern, #[[1]]] &];
+(* Why do we add a plus 1 here? - Because that is the width of the blocks *)
+Max[#[[2]]&/@columns]+1]
+
+
+simulateVerticalGrowth[iterations_, pieces_, width_]:=Module[{i, objects={}, currentList={}, x, new, columnsAffected, itemsEffected, highestFoundation },
+For[i=1,i<iterations, i++,
+(* Label[begin] *)
+(* Determine x coordinate of new piece *)
+x=Ceiling[width*Random[]];
+new=({x, 0}+#)&/@pieces[[Round[Random[]] + 1 ]];
+(* Find all the x coordinates that are relevant to stack on *)
+columnsAffected=Complement[#[[1]]&/@new];
+(* Determine if there is any existing items in the foundation that the new piece will interact with *) 
+itemsEffected=Select[currentList, Length[Intersection[#[[1]]&/@#,columnsAffected]]>0 &];
+(* If there are no items effected, add to the currentList and define the next piece *)
+highestFoundation=If[Length[itemsEffected]==0, 0, Max[getMaxY[#, columnsAffected]&/@itemsEffected]];
+currentList=If[Length[itemsEffected]==0, Append[currentList, new], Append[currentList, ({0, highestFoundation} +#)&/@new]];];
+currentList]
+
+
+makePiece[x_, y_]:=Partition[Flatten[Table[Table[{i-1,j-1}, {i, 1, x}], {j, 1, y}]], 2]
